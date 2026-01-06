29c778aff0cc3c2b8daf0d9fd03c33ac0aa17a66
enybcode <enzo.berbertrand@gmail.com>
2026-01-06 18:35:05 +0100

    regeneration pills

--- src/main/java/net/mcreator/murimblock/item/PillsItem.java
+++ src/main/java/net/mcreator/murimblock/item/PillsItem.java
@@ -1,18 +1,31 @@
 package net.mcreator.murimblock.item;
 
+import net.minecraft.world.level.Level;
 import net.minecraft.world.item.component.ItemAttributeModifiers;
 import net.minecraft.world.item.component.Consumables;
 import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.EquipmentSlotGroup;
+
+import net.mcreator.murimblock.procedures.PillsPlayerFinishesUsingItemProcedure;
 
 public class PillsItem extends Item {
 	public PillsItem(Item.Properties properties) {
 		super(properties.rarity(Rarity.UNCOMMON).stacksTo(12).fireResistant().food((new FoodProperties.Builder()).nutrition(3).saturationModifier(0.3f).alwaysEdible().build(), Consumables.defaultFood().consumeSeconds(2.5F).build())
 				.attributes(ItemAttributeModifiers.builder().add(Attributes.ATTACK_DAMAGE, new AttributeModifier(BASE_ATTACK_DAMAGE_ID, 0, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND)
-						.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.4, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build()));
+						.add(Attributes.ATTACK_SPEED, new AttributeModifier(BASE_ATTACK_SPEED_ID, -2.4, AttributeModifier.Operation.ADD_VALUE), EquipmentSlotGroup.MAINHAND).build())
+				.enchantable(1));
+	}
+
+	@Override
+	public ItemStack finishUsingItem(ItemStack itemstack, Level world, LivingEntity entity) {
+		ItemStack retval = super.finishUsingItem(itemstack, world, entity);
+		PillsPlayerFinishesUsingItemProcedure.execute(entity);
+		return retval;
 	}
 }
