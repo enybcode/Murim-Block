c1c30af22419f696f4ccab8d1125d6fbd91f4ed7
enybcode <enzo.berbertrand@gmail.com>
2026-01-06 18:51:57 +0100

    Add qui to pill

--- src/main/java/net/mcreator/murimblock/procedures/PillsPlayerFinishesUsingItemProcedure.java
+++ src/main/java/net/mcreator/murimblock/procedures/PillsPlayerFinishesUsingItemProcedure.java
@@ -1,15 +1,21 @@
 package net.mcreator.murimblock.procedures;
 
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.server.level.ServerLevel;
+
+import net.mcreator.murimblock.init.MurimBlockModGameRules;
 
 public class PillsPlayerFinishesUsingItemProcedure {
-	public static void execute(Entity entity) {
+	public static void execute(LevelAccessor world, Entity entity) {
 		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 60, 1, true, false));
+		if (world instanceof ServerLevel _serverLevel)
+			_serverLevel.getGameRules().getRule(MurimBlockModGameRules.QI).set((world instanceof ServerLevel _serverLevelGR1 ? _serverLevelGR1.getGameRules().getInt(MurimBlockModGameRules.QI) : 0) + 10, world.getServer());
 	}
 }
