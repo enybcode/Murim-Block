a77e4dff6ce6eb6f5abd0109616950acfe42c2f9
enybcode <enzo.berbertrand@gmail.com>
2026-01-08 21:03:49 +0100

    yes

--- src/main/java/net/mcreator/murimblock/item/PillsItem.java
+++ src/main/java/net/mcreator/murimblock/item/PillsItem.java
@@ -1,5 +1,19 @@
 package net.mcreator.murimblock.item;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.item.component.ItemAttributeModifiers;
+import net.minecraft.world.item.component.Consumables;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.AttributeModifier;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlotGroup;
+
+import net.mcreator.murimblock.procedures.PillsPlayerFinishesUsingItemProcedure;
+
 public class PillsItem extends Item {
 	public PillsItem(Item.Properties properties) {
 		super(properties.rarity(Rarity.UNCOMMON).stacksTo(12).fireResistant().food((new FoodProperties.Builder()).nutrition(3).saturationModifier(0.3f).alwaysEdible().build(), Consumables.defaultFood().consumeSeconds(2.5F).build())
