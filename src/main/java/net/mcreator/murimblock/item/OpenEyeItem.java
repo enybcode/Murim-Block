9fb227c48ee8ab54661acf4c69e8c69457d29f43
RobinBVLT <Robin@DESKTOP-NORN1TA>
2026-01-08 18:32:49 +0100

    Changement du logo du succés open eyes et création d'un fichier item succée pour les logos

---
+++ src/main/java/net/mcreator/murimblock/item/OpenEyeItem.java
@@ -0,0 +1,9 @@
+package net.mcreator.murimblock.item;
+
+import net.minecraft.world.item.Item;
+
+public class OpenEyeItem extends Item {
+	public OpenEyeItem(Item.Properties properties) {
+		super(properties);
+	}
+}
