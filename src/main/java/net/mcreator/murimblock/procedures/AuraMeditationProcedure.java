4ec8cf604a7d03b8d122224e3e354356ab4fccff
enybcode <enzo.berbertrand@gmail.com>
2026-01-08 22:51:43 +0100

    All meditation type shit

--- src/main/java/net/mcreator/murimblock/procedures/AuraMeditationProcedure.java
+++ src/main/java/net/mcreator/murimblock/procedures/AuraMeditationProcedure.java
@@ -6,11 +6,16 @@ import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.bus.api.Event;
 
 import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.particles.ParticleTypes;
+
+import net.mcreator.murimblock.network.MurimBlockModVariables;
 
 import javax.annotation.Nullable;
 
@@ -28,12 +33,31 @@ public class AuraMeditationProcedure {
 	private static void execute(@Nullable Event event, LevelAccessor world, double x, double y, double z, Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof ServerPlayer _plr0 && _plr0.level() instanceof ServerLevel _serverLevel0
-				&& _plr0.getAdvancements().getOrStartProgress(_serverLevel0.getServer().getAdvancements().get(ResourceLocation.parse("murim_block:open_eyes"))).isDone()) {
-			if (world instanceof ServerLevel _level)
-				_level.sendParticles(ParticleTypes.SOUL, x, y, z, 10, 0.5, 1.8, 0.4, 0.05);
-			if (world instanceof ServerLevel _level)
-				_level.sendParticles(ParticleTypes.SOUL_FIRE_FLAME, x, y, z, 10, 0.2, 1.8, 0.4, 0.05);
+		if (entity.getData(MurimBlockModVariables.PLAYER_VARIABLES).AuraActive == true) {
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.RESISTANCE, 40, 2, false, false));
+			if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
+				_entity.addEffect(new MobEffectInstance(MobEffects.SLOWNESS, 40, 10, false, false));
+			if (entity instanceof ServerPlayer _plr2 && _plr2.level() instanceof ServerLevel _serverLevel2
+					&& _plr2.getAdvancements().getOrStartProgress(_serverLevel2.getServer().getAdvancements().get(ResourceLocation.parse("murim_block:open_eyes"))).isDone()) {
+				if (world instanceof ServerLevel _level)
+					_level.sendParticles(ParticleTypes.SOUL, x, y, z, 10, 0.5, 1.8, 0.4, 0.05);
+				if (world instanceof ServerLevel _level)
+					_level.sendParticles(ParticleTypes.SOUL_FIRE_FLAME, x, y, z, 10, 0.2, 1.8, 0.4, 0.05);
+			}
+			{
+				MurimBlockModVariables.PlayerVariables _vars = entity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
+				_vars.QiTimer = entity.getData(MurimBlockModVariables.PLAYER_VARIABLES).QiTimer + 1;
+				_vars.markSyncDirty();
+			}
+			if (entity.getData(MurimBlockModVariables.PLAYER_VARIABLES).QiTimer >= 200) {
+				{
+					MurimBlockModVariables.PlayerVariables _vars = entity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
+					_vars.Qi = entity.getData(MurimBlockModVariables.PLAYER_VARIABLES).Qi * 1.03;
+					_vars.QiTimer = 0;
+					_vars.markSyncDirty();
+				}
+			}
 		}
 	}
 }
