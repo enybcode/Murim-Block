3237d2aeb4220a66d30458cedb054edcdcf3f14d
enybcode <enzo.berbertrand@gmail.com>
2026-01-08 20:43:42 +0100

    commande check qi

---
+++ src/main/java/net/mcreator/murimblock/procedures/CheckQiCommandProcedure.java
@@ -0,0 +1,31 @@
+package net.mcreator.murimblock.procedures;
+
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.chat.Component;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.commands.CommandSourceStack;
+
+import net.mcreator.murimblock.network.MurimBlockModVariables;
+
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.context.CommandContext;
+
+public class CheckQiCommandProcedure {
+	public static void execute(CommandContext<CommandSourceStack> arguments, Entity entity) {
+		if (entity == null)
+			return;
+		if (entity instanceof Player _player && !_player.level().isClientSide())
+			_player.displayClientMessage(
+					Component.literal(("The Qi of" + (commandParameterEntity(arguments, "target")).getDisplayName().getString() + "is" + (commandParameterEntity(arguments, "target")).getData(MurimBlockModVariables.PLAYER_VARIABLES).Qi)), false);
+	}
+
+	private static Entity commandParameterEntity(CommandContext<CommandSourceStack> arguments, String parameter) {
+		try {
+			return EntityArgument.getEntity(arguments, parameter);
+		} catch (CommandSyntaxException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
