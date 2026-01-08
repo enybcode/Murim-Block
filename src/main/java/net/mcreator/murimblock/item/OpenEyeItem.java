05a18a30b48d89ae080af4b3f0fb28b757a933c6
enybcode <enzo.berbertrand@gmail.com>
2026-01-08 19:25:17 +0100

    modification pill

--- src/main/java/net/mcreator/murimblock/item/OpenEyeItem.java
+++ src/main/java/net/mcreator/murimblock/item/OpenEyeItem.java
@@ -1,5 +1,7 @@
 package net.mcreator.murimblock.item;
 
+import net.minecraft.world.item.Item;
+
 public class OpenEyeItem extends Item {
 	public OpenEyeItem(Item.Properties properties) {
 		super(properties);
--- src/main/java/net/mcreator/murimblock/procedures/PillsPlayerFinishesUsingItemProcedure.java
+++ src/main/java/net/mcreator/murimblock/procedures/PillsPlayerFinishesUsingItemProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.murimblock.procedures;
 
+import net.neoforged.neoforge.event.entity.living.LivingEntityUseItemEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.effect.MobEffects;
@@ -7,15 +12,29 @@ import net.minecraft.world.effect.MobEffectInstance;
 
 import net.mcreator.murimblock.network.MurimBlockModVariables;
 
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
 public class PillsPlayerFinishesUsingItemProcedure {
-	public static void execute(Entity entity, Entity sourceentity) {
-		if (entity == null || sourceentity == null)
+	@SubscribeEvent
+	public static void onUseItemFinish(LivingEntityUseItemEvent.Finish event) {
+		if (event.getEntity() != null) {
+			execute(event, event.getEntity());
+		}
+	}
+
+	public static void execute(Entity entity) {
+		execute(null, entity);
+	}
+
+	private static void execute(@Nullable Event event, Entity entity) {
+		if (entity == null)
 			return;
 		if (entity instanceof LivingEntity _entity && !_entity.level().isClientSide())
 			_entity.addEffect(new MobEffectInstance(MobEffects.REGENERATION, 60, 1, true, false));
 		{
-			MurimBlockModVariables.PlayerVariables _vars = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
-			_vars.Qi = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES).Qi + 10;
+			MurimBlockModVariables.PlayerVariables _vars = entity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
+			_vars.Qi = entity.getData(MurimBlockModVariables.PLAYER_VARIABLES).Qi + 10;
 			_vars.markSyncDirty();
 		}
 	}
