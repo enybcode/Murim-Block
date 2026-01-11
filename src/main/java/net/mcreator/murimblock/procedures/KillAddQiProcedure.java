afa01882148e5ef7ab96404655522ba34f7fe013
enybcode <enzo.berbertrand@gmail.com>
2026-01-11 01:34:00 +0100

    changement mineur kill add qi

--- src/main/java/net/mcreator/murimblock/procedures/KillAddQiProcedure.java
+++ src/main/java/net/mcreator/murimblock/procedures/KillAddQiProcedure.java
@@ -1,5 +1,10 @@
 package net.mcreator.murimblock.procedures;
 
+import net.neoforged.neoforge.event.entity.living.LivingDeathEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.entity.Mob;
@@ -11,20 +16,42 @@ import net.minecraft.core.particles.ParticleTypes;
 
 import net.mcreator.murimblock.network.MurimBlockModVariables;
 
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
 public class KillAddQiProcedure {
+	@SubscribeEvent
+	public static void onEntityDeath(LivingDeathEvent event) {
+		if (event.getEntity() != null) {
+			execute(event, event.getEntity().level(), event.getEntity(), event.getSource().getEntity());
+		}
+	}
+
 	public static void execute(LevelAccessor world, Entity entity, Entity sourceentity) {
+		execute(null, world, entity, sourceentity);
+	}
+
+	private static void execute(@Nullable Event event, LevelAccessor world, Entity entity, Entity sourceentity) {
 		if (entity == null || sourceentity == null)
 			return;
-		if (sourceentity instanceof ServerPlayer _plr0 && _plr0.level() instanceof ServerLevel _serverLevel0
-				&& _plr0.getAdvancements().getOrStartProgress(_serverLevel0.getServer().getAdvancements().get(ResourceLocation.parse("murim_block:open_eyes"))).isDone()) {
-			if (sourceentity instanceof Player == (entity instanceof Mob _mobEnt2 && _mobEnt2.isAggressive())) {
-				{
-					MurimBlockModVariables.PlayerVariables _vars = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
-					_vars.QiMax = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES).QiMax + 5;
-					_vars.markSyncDirty();
+		if (sourceentity instanceof Player) {
+			if (sourceentity instanceof ServerPlayer _plr1 && _plr1.level() instanceof ServerLevel _serverLevel1
+					&& _plr1.getAdvancements().getOrStartProgress(_serverLevel1.getServer().getAdvancements().get(ResourceLocation.parse("murim_block:open_eyes"))).isDone()) {
+				if (entity instanceof Mob _mobEnt2 && _mobEnt2.isAggressive()) {
+					{
+						MurimBlockModVariables.PlayerVariables _vars = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
+						_vars.QiMax = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES).QiMax + 5;
+						_vars.markSyncDirty();
+					}
+					if (world instanceof ServerLevel _level)
+						_level.sendParticles(ParticleTypes.LAVA, (sourceentity.getX()), (sourceentity.getY()), (sourceentity.getZ()), 5, 0, 0, 0, 10);
+				} else {
+					{
+						MurimBlockModVariables.PlayerVariables _vars = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES);
+						_vars.QiMax = sourceentity.getData(MurimBlockModVariables.PLAYER_VARIABLES).QiMax + 1;
+						_vars.markSyncDirty();
+					}
 				}
-				if (world instanceof ServerLevel _level)
-					_level.sendParticles(ParticleTypes.LAVA, (sourceentity.getX()), (sourceentity.getY()), (sourceentity.getZ()), 5, 0, 0, 0, 10);
 			}
 		}
 	}
