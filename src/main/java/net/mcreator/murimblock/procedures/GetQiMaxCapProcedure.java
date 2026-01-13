55a6fabdcb2ad8ae087b90689791766aec80704e
enybcode <enzo.berbertrand@gmail.com>
2026-01-13 10:05:16 +0100

    essai

--- src/main/java/net/mcreator/murimblock/procedures/GetQiMaxCapProcedure.java
+++ src/main/java/net/mcreator/murimblock/procedures/GetQiMaxCapProcedure.java
@@ -1,11 +1,28 @@
 package net.mcreator.murimblock.procedures;
 
+import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.bus.api.Event;
+
 import net.minecraft.world.entity.Entity;
 
 import net.mcreator.murimblock.network.MurimBlockModVariables;
 
+import javax.annotation.Nullable;
+
+@EventBusSubscriber
 public class GetQiMaxCapProcedure {
+	@SubscribeEvent
+	public static void onPlayerTick(PlayerTickEvent.Post event) {
+		execute(event, event.getEntity());
+	}
+
 	public static double execute(Entity entity) {
+		return execute(null, entity);
+	}
+
+	private static double execute(@Nullable Event event, Entity entity) {
 		if (entity == null)
 			return 0;
 		if (1 == entity.getData(MurimBlockModVariables.PLAYER_VARIABLES).CultivationStage) {
