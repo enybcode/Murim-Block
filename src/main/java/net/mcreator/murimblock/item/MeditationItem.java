8db6c36400c4b092519ae23d836b1bd99639a7e3
RobinBVLT <Robin@DESKTOP-NORN1TA>
2026-01-16 14:01:42 +0100

    ajout icon méditation

---
+++ src/main/java/net/mcreator/murimblock/item/MeditationItem.java
@@ -0,0 +1,9 @@
+package net.mcreator.murimblock.item;
+
+import net.minecraft.world.item.Item;
+
+public class MeditationItem extends Item {
+	public MeditationItem(Item.Properties properties) {
+		super(properties);
+	}
+}
